29e0ead69361bbab259d79582fe5801656172bb5
MaxProPlus <mutasik170300@gmail.com>
2020-07-23 17:53:32 +0400

    Сжат бекраунд сайта; обновлен скрипт деплоя

--- release.sh
+++ release.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-ssh servT 'rm -rf ~/wow/server/src'
-scp -r './server/src/' servT:~/wow/server/
-ssh servT 'cd ~/wow/server/src && npm run build'
-
-ssh servT 'rm -rf ~/wow/client/src'
-scp -r './client/src/' servT:~/wow/client/
-ssh servT 'cd ~/wow/client/src && npm run build'
-
-# ssh serverT 'cd ~/wow && git reset --hard && git fetch && git rebase'
-# ssh serverT 'cd ~/wow/server && npm i && npm run build'
-# ssh serverT 'cd ~/wow/client && npm i && npm run build'
-# ssh serverT 'cd ~/wow/server && nohup npm run start > /dev/null 2>&1 &'
+ssh servT 'cd ~/wow && git reset --hard && git fetch && git rebase'
+ssh servT 'cd ~/wow/server && npm i && npm run build'
+ssh servT 'cd ~/wow/client && npm i && npm run build'
+ssh servT 'cd ~/wow/server && nohup npm run start > /dev/null 2>&1 &'
 

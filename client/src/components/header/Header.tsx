d27218c492b0d0e8f570b9e96223d7a642aeb446
MaxProPlus <mutasik170300@gmail.com>
2020-08-10 23:51:53 +0400

    Фикс хедера, добавление анимации к хедеру

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -5,7 +5,7 @@ import logo from './logo.png'
 import Accordion from "../accordion/Accordion"
 import history from "../../utils/history"
 
-type IProps = {
+type P = {
     showMenu: boolean
     innerRef: any
     hideMenu: any
@@ -18,7 +18,7 @@ type S = {
     path: string
 }
 
-class Header extends Component<IProps, S> {
+class Header extends Component<P, S> {
     private removeListen: any
 
     constructor(props: any) {
@@ -47,7 +47,9 @@ class Header extends Component<IProps, S> {
     toggleStart = () => {
         this.setState((state: S) => {
             return {
-                showStart: !state.showStart
+                showStart: !state.showStart,
+                showMaterial: false,
+                showHelp: false,
             }
         })
 
@@ -56,7 +58,9 @@ class Header extends Component<IProps, S> {
     toggleMaterial = () => {
         this.setState((state: S) => {
             return {
-                showMaterial: !state.showMaterial
+                showMaterial: !state.showMaterial,
+                showStart: false,
+                showHelp: false,
             }
         })
 
@@ -65,7 +69,9 @@ class Header extends Component<IProps, S> {
     toggleHelp = () => {
         this.setState((state: S) => {
             return {
-                showHelp: !state.showHelp
+                showHelp: !state.showHelp,
+                showStart: false,
+                showMaterial: false,
             }
         })
 
@@ -75,72 +81,72 @@ class Header extends Component<IProps, S> {
         return (
             <header ref={this.props.innerRef}
                     className={this.props.showMenu ? "fc header show" : "fc header"}>
-                <div className="header-inner">
-                    <Link onClick={this.props.hideMenu} className="fc logo" to="/"><img src={logo}
-                                                                                        alt="Equilibrium"/></Link>
-                    <div className={`header-item${this.state.path.includes('/start') ? ' item-active' : ''}`}
-                         onClick={this.toggleStart}>
-                        Начать игру
-                    </div>
-                    <Accordion isActive={this.state.showStart}>
-                        <div onClick={this.props.hideMenu} className="header-sub">
-                            <Link
-                                className={`header-item sub-item${this.state.path.includes('/reg') ? ' item-active' : ''}`}
-                                to="/reg">Регистрация</Link>
-                            <Link
-                                className={`header-item sub-item${this.state.path.includes('/rule') ? ' item-active' : ''}`}
-                                to="/rule">Правила</Link>
-                            <Link
-                                className={`header-item sub-item${this.state.path.includes('/how') ? ' item-active' : ''}`}
-                                to="/how">Как играть?</Link>
+                    <div className="header-inner">
+                        <Link onClick={this.props.hideMenu} className="fc logo" to="/"><img src={logo}
+                                                                                            alt="Equilibrium"/></Link>
+                        <div className={`header-item${this.state.path.includes('/start') ? ' item-active' : ''}`}
+                             onClick={this.toggleStart}>
+                            Начать игру
                         </div>
-                    </Accordion>
+                        <Accordion isActive={this.state.showStart}>
+                            <div onClick={this.props.hideMenu} className="header-sub">
+                                <Link
+                                    className={`header-item sub-item${this.state.path.includes('/reg') ? ' item-active' : ''}`}
+                                    to="/reg">Регистрация</Link>
+                                <Link
+                                    className={`header-item sub-item${this.state.path.includes('/rule') ? ' item-active' : ''}`}
+                                    to="/rule">Правила</Link>
+                                <Link
+                                    className={`header-item sub-item${this.state.path.includes('/how') ? ' item-active' : ''}`}
+                                    to="/how">Как играть?</Link>
+                            </div>
+                        </Accordion>
 
-                    <Link onClick={this.props.hideMenu}
-                          className={`header-item${this.state.path.includes('/news') ? ' item-active' : ''}`}
-                          to="/news">Новости</Link>
+                        <Link onClick={this.props.hideMenu}
+                              className={`header-item${this.state.path.includes('/news') ? ' item-active' : ''}`}
+                              to="/news">Новости</Link>
 
-                    <div
-                        className={`header-item${this.state.path.includes('/material') ? ' item-active' : ''}`}
-                        onClick={this.toggleMaterial}>
-                        Материалы
-                    </div>
-                    <Accordion isActive={this.state.showMaterial}>
-                        <div onClick={this.props.hideMenu} className="header-sub">
-                            <Link
-                                className={`header-item sub-item${this.state.path.includes('/character') ? ' item-active' : ''}`}
-                                to="/material/character">Персонажи</Link>
-                            <Link
-                                className={`header-item sub-item${this.state.path.includes('/guild') ? ' item-active' : ''}`}
-                                to="/material/guild">Гильдии</Link>
-                            <Link
-                                className={`header-item sub-item${this.state.path.includes('/story') ? ' item-active' : ''}`}
-                                to="/material/story">Сюжет</Link>
-                            <Link
-                                className={`header-item sub-item${this.state.path.includes('/report') ? ' item-active' : ''}`}
-                                to="/material/report">Отчеты</Link>
-                            <Link
-                                className={`header-item sub-item${this.state.path.includes('/guid') ? ' item-active' : ''}`}
-                                to="/material/guid">Гайды</Link>
-                            <Link
-                                className={`header-item sub-item${this.state.path.includes('/forum') ? ' item-active' : ''}`}
-                                to="/material/forum">Форум</Link>
+                        <div
+                            className={`header-item${this.state.path.includes('/material') ? ' item-active' : ''}`}
+                            onClick={this.toggleMaterial}>
+                            Материалы
                         </div>
-                    </Accordion>
+                        <Accordion isActive={this.state.showMaterial}>
+                            <div onClick={this.props.hideMenu} className="header-sub">
+                                <Link
+                                    className={`header-item sub-item${this.state.path.includes('/character') ? ' item-active' : ''}`}
+                                    to="/material/character">Персонажи</Link>
+                                <Link
+                                    className={`header-item sub-item${this.state.path.includes('/guild') ? ' item-active' : ''}`}
+                                    to="/material/guild">Гильдии</Link>
+                                <Link
+                                    className={`header-item sub-item${this.state.path.includes('/story') ? ' item-active' : ''}`}
+                                    to="/material/story">Сюжет</Link>
+                                <Link
+                                    className={`header-item sub-item${this.state.path.includes('/report') ? ' item-active' : ''}`}
+                                    to="/material/report">Отчеты</Link>
+                                <Link
+                                    className={`header-item sub-item${this.state.path.includes('/guid') ? ' item-active' : ''}`}
+                                    to="/material/guid">Гайды</Link>
+                                <Link
+                                    className={`header-item sub-item${this.state.path.includes('/forum') ? ' item-active' : ''}`}
+                                    to="/material/forum">Форум</Link>
+                            </div>
+                        </Accordion>
 
-                    <div className={`header-item${this.state.path.includes('/help') ? ' item-active' : ''}`}
-                         onClick={this.toggleHelp}>
-                        Помощь
-                    </div>
-                    <Accordion isActive={this.state.showHelp}>
-                        <div onClick={this.props.hideMenu} className="header-sub">
-                            <Link
-                                className={`header-item sub-item${this.state.path.includes('/ticket') ? ' item-active' : ''}`}
-                                to="/help/ticket/type">Обратная связь</Link>
+                        <div className={`header-item${this.state.path.includes('/help') ? ' item-active' : ''}`}
+                             onClick={this.toggleHelp}>
+                            Помощь
                         </div>
-                    </Accordion>
+                        <Accordion isActive={this.state.showHelp}>
+                            <div onClick={this.props.hideMenu} className="header-sub">
+                                <Link
+                                    className={`header-item sub-item${this.state.path.includes('/ticket') ? ' item-active' : ''}`}
+                                    to="/help/ticket/type">Обратная связь</Link>
+                            </div>
+                        </Accordion>
 
-                </div>
+                    </div>
             </header>
         )
     }

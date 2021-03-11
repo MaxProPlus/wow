881509cc207e9b13eab7b1592179f83fa622b90b
MaxProPlus <mutasik170300@gmail.com>
2021-03-11 19:33:25 +0400

    Остановка сервера по сигналам Unix

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,30 @@ import app from './server'
 
 // Start the server
 const port = configProvider.get<number>('port')
-app.listen(port, () => {
+const server = app.listen(port, () => {
   logger.info(
     `Express server started on : http://${configProvider.get<string>(
       'host'
     )}:${port}`
   )
 })
+
+// Stop the server
+const stop = () => {
+  logger.info('Interrupted')
+  if (!server.listening) {
+    process.exit(0)
+  }
+
+  server.close((err) => {
+    if (err) {
+      logger.error('Error on close', err)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
+}
+
+// Stop on signal
+process.on('SIGINT', stop)
+process.on('SIGTERM', stop)
